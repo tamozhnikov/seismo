5f77512c09132711d53e89c8c6fdc63f0bc458a6
Artem Tamozhnikov <tamozhnikov@ngs.ru>
2023-07-07 19:03:07 +0700

    collector: Add working with mongodb (getting last message time for source and saving messages)

---
+++ collector/db/mongodb/adapter.go
@@ -0,0 +1,87 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"path"
+	"seismo/provider"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	msgCollName = "messages"
+)
+
+type Adapter struct {
+	connStr string
+	dbName  string
+	client  mongo.Client
+}
+
+func (a *Adapter) Connect(ctx context.Context, connStr string) error {
+	c, err := mongo.Connect(ctx, options.Client().ApplyURI(connStr))
+	if err != nil {
+		return fmt.Errorf("Connect: error: %w", err)
+	}
+	a.connStr = connStr
+	a.dbName = path.Base(connStr)
+	a.client = *c
+	return nil
+}
+
+func (a *Adapter) Close(ctx context.Context) error {
+	err := a.client.Disconnect(ctx)
+	if err != nil {
+		return fmt.Errorf("Close: error: %w", err)
+	}
+	return nil
+}
+
+func (a *Adapter) SaveMsg(ctx context.Context, msgs []provider.Message) error {
+	coll := a.client.Database(a.dbName).Collection(msgCollName)
+	mi := make([]interface{}, 0, len(msgs))
+	for _, m := range msgs {
+		mi = append(mi, m)
+	}
+	_, err := coll.InsertMany(ctx, mi)
+	if err != nil {
+		return fmt.Errorf("SaveMsg: error: %w", err)
+	}
+
+	return nil
+}
+
+func (a *Adapter) GetLastTime(ctx context.Context, sourceId string) (time.Time, error) {
+	coll := a.client.Database(a.dbName).Collection(msgCollName)
+
+	//db.messages.aggregate([{$match: {sourceid: "pseudo_1"}},  {$group : { _id: "$source_id", max_magn: {$max : "$focustime"}}}])
+	cur, err := coll.Aggregate(ctx, mongo.Pipeline{
+		{{"$match", bson.D{{"source_id", sourceId}}}},
+		{{"$group", bson.D{{"_id", "$source_id"}, {"time", bson.D{{"$max", "$focus_time"}}}}}},
+	})
+
+	if err != nil {
+		return time.Time{}, fmt.Errorf("GetLastTime: error: %w", err)
+	}
+	var res []bson.M
+	if err = cur.All(ctx, &res); err != nil {
+		return time.Time{}, fmt.Errorf("GetLastTime: error: %w", err)
+	}
+
+	if len(res) < 1 {
+		return time.Time{}, nil
+	}
+
+	i := res[0]["time"]
+	t, ok := i.(primitive.DateTime)
+	if !ok {
+		return time.Time{}, fmt.Errorf("GetLastTime: unexpected type for time %v; DateTime type is exected", res[0]["time"])
+	}
+
+	return t.Time(), nil
+}
---
+++ collector/db/mongodb/adapter_test.go
@@ -0,0 +1,20 @@
+package mongodb
+
+import (
+	"context"
+	"testing"
+)
+
+func Test_GetLastTim(t *testing.T) {
+	//t.Error()
+	var a Adapter
+	err := a.Connect(context.TODO(), "mongodb://localhost:27017/collectorDb")
+	if err != nil {
+		t.Fatalf("cannot connect to the database")
+	}
+
+	_, err = a.GetLastTime(context.TODO(), "pseudo_1")
+	if err != nil {
+		t.Errorf("GetLastTime error: %v", err)
+	}
+}
